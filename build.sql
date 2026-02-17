b3db4d82a52579eed989e28bed4690029fe2dbfc
venuoleti32 <venuoleti32@outlook.com>
2026-02-16 21:47:48 -0600

    Update build.sql

--- build.sql
+++ build.sql
@@ -36,3 +36,15 @@ CREATE OR REPLACE FILE FORMAT my_csv_format
     volume NUMBER,
     ingested_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
 );
+
+CREATE TABLE IF NOT EXISTS financial_db.raw_ingestion.silver_stock_prices (
+    symbol STRING,
+    trade_date DATE,
+    open_price FLOAT,
+    high_price FLOAT,
+    low_price FLOAT,
+    close_price FLOAT,
+    volume NUMBER,
+    ingested_at TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP(),
+    PRIMARY KEY (symbol, trade_date)
+);
