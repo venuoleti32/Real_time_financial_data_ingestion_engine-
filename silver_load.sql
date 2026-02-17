ec7d77044ee8a39c879d0f484c90b9e200475f8c
venuoleti32 <venuoleti32@outlook.com>
2026-02-16 22:32:12 -0600

    Update silver_load.sql

--- silver_load.sql
+++ silver_load.sql
@@ -1,16 +1,16 @@
-
-
-COPY INTO financial_db.raw_ingestion.silver_stock_prices
+-- scripts/silver_load.sql
+COPY INTO financial_db.raw_ingestion.silver_stock_prices 
+(SYMBOL, TRADE_DATE, OPEN_PRICE, HIGH_PRICE, LOW_PRICE, CLOSE_PRICE, VOLUME) -- We list only 7 columns
 FROM (
   SELECT 
-    'IBM',               
-    $1::DATE,            
-    $2::FLOAT,           
-    $3::FLOAT,           
-    $4::FLOAT,           
-    $5::FLOAT,           
-    $6::NUMBER           
-  FROM @stock_data_stage
+    'IBM',               -- 1
+    $1::DATE,            -- 2
+    $2::FLOAT,           -- 3
+    $3::FLOAT,           -- 4
+    $4::FLOAT,           -- 5
+    $5::FLOAT,           -- 6
+    $6::NUMBER           -- 7
+  FROM @FINANCIAL_DB.RAW_INGESTION.STOCK_DATA_STAGE
 )
 FILE_FORMAT = (FORMAT_NAME = 'my_csv_format')
-ON_ERROR = 'CONTINUE';   
+ON_ERROR = 'CONTINUE';
